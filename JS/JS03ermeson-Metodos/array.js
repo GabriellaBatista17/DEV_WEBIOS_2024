eb7e7b8fe24deeb4a843526f7a05ca8dcd3939a9
GabriellaBatista17 <batistagabi800@gmail.com>
2024-10-17 21:16:25 -0300

    JS03

---
+++ JS/JS03ermeson-Metodos/array.js
@@ -0,0 +1,26 @@
+// let grupo3 = [`Gabriella`, `Giovani`, `Wallyson`, `Eugênio`, `Sara`]
+// //              0               1        2            3         4
+// console.log(typeof grupo3[0]);
+// console.log(grupo03 [0])
+
+// let carro = [`Corola`, `Sandero`, `Gol`, `Fiat`, `Ferrari`]
+// console.log(carro [4]);
+// console.log(carro [3]);
+
+
+
+// let cupons = 'DESC10' //10%
+// //  .nome()
+// let menor = cupons.toLowerCase()
+// console.log(menor);
+
+// let maior = cupons.toUpperCase()
+// console.log(maior); 
+
+// let IMC = 19.937889474
+// let novoValor = IMC.toFixed(2) // para diminuir os numeros após a virgula
+// console.log(IMC);
+
+
+
+
---
+++ JS/JS03ermeson-Metodos/substring&slipt.js
@@ -0,0 +1,12 @@
+// let navegador = 'Brave'
+// let novaString = navegador.substring(2)
+// let novaSubstring = navegador.substring(1,3) //para separar o que você qr das outras
+// let sting = navegador.charAt(3) //para uma letra especifica
+// console.log(sting);
+// console.log(novaString);
+// console.log(novaSubstring);
+
+let frase = 'Na moral mano!'
+console.log(frase);
+let novaF = frase.split(" ")
+console.log(novaF); //para fazer array a partir de um criterio q vc determinar
