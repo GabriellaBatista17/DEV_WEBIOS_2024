7f9efc4336e89ded34e4a60a03f7faa3b37bf183
GabriellaBatista17 <batistagabi800@gmail.com>
2024-10-21 21:42:09 -0300

    JS04

---
+++ JS/JS04-Condicional/main.js
@@ -0,0 +1,25 @@
+//verificar se o Caio está com a camiseta do IOS
+
+let caioComBlusa = false
+if(caioComBlusa == true){
+    console.log("Caio pode assistir a aula");
+} else{ // falso
+    console.log("Vai pa casa");
+}
+console.clear()
+
+//um jogador somente irá para a fase 2 se atingir 1000pts
+
+let pontos = 1000
+if(pontos >= 1000){ //sempre que for true, vai executar o bloco de cod
+    console.log("Passou para a fase 2");
+}
+console.clear()
+
+//se o resto da divisão por 2 for igual a 0 é Par, mas se for 1 é impar
+let imparPar = 18
+if(imparPar % 2 == 0 ){ //resulatado true
+    console.log("Par");
+} else{
+    console.log("Impar");
+}
