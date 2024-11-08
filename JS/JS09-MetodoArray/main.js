2aa5b90e6fe470f0b473360a125d2b3aa8069acb
GabriellaBatista17 <batistagabi800@gmail.com>
2024-11-07 21:24:18 -0300

    atividade casa

---
+++ JS/JS09-MetodoArray/main.js
@@ -0,0 +1,14 @@
+let frutas = [ "morango", "banana", "melancia", "lichia", "limão", "abacaxi"," melão", "maçã"]
+
+frutas.forEach(Percorrer)
+
+//                 conteudo  local
+function Percorrer(valor, indice){
+    console.log(valor);
+    console.log(indice);
+}
+
+// for(i= 0; i <= frutas.length ; i++){
+// console.log(frutas[i]); // conteudo/valor da posição
+// console.log(i); //numero da posição
+// }
